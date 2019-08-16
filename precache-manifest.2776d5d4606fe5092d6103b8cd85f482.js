1b86acbbda779ac4fcd6e7becbc64f9566f2199a
Dariia Zhyr <darzhyr@gmail.com>
2019-08-16 17:16:52 +0300

    Updates

---
+++ precache-manifest.2776d5d4606fe5092d6103b8cd85f482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00f56f7453a6f2a1fb465d2e1a03132",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "2c97dad4dd32af3ff75f",
+    "url": "/todo-list/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "137e149705fc93129642",
+    "url": "/todo-list/static/js/2.597e021f.chunk.js"
+  },
+  {
+    "revision": "2c97dad4dd32af3ff75f",
+    "url": "/todo-list/static/js/main.3c948348.chunk.js"
+  },
+  {
+    "revision": "51236a9f9133e160b8c3",
+    "url": "/todo-list/static/js/runtime~main.fb7e4658.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
